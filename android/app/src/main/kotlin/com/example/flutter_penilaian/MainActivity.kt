117e63dff6d6df2af3d4a275a2f23464c0d25ba1
samuelrvld <samuelrvld@gmail.com>
2024-11-18 14:23:18 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_penilaian/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_penilaian
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/history_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/kehadiran_provider.dart';
+
+class HistoryPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<KehadiranProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Riwayat Kehadiran'),
+      backgroundColor: Colors.yellowAccent),
+      body: ListView.builder(
+        itemCount: provider.history.length,
+        itemBuilder: (context, index) {
+          final record = provider.history[index];
+          return ListTile(
+            title: Text(record.date),
+            subtitle: Text('Hadir: ${record.presentCount}, Tidak Hadir: ${record.absentCount}'),
+          );
+        },
+      ),
+    );
+  }
+}
